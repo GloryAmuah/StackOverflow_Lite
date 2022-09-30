b95f67acf2343f5e55d85059295780f1efd91040
Glory Amuah <glory.amuah@flutterwavego.com>
2022-09-30 07:52:03 +0100

    added test files

--- routes/questions.js
+++ routes/questions.js
@@ -12,11 +12,5 @@ router.delete('/:id', checkAuthMiddleware.checkAuth, questionsController.deleteQ
 router.post('/:id/answers', checkAuthMiddleware.checkAuth, answersController.addAnswer)//add an answer to a question
 router.get('/:id/answers', answersController.fetchAnswers)//fetch all answers to a question
 
-//const router = express.Router();
-
-// router.post('/', checkAuthMiddleware.checkAuth, questionsController.addQuestion)//add a question
-// router.get('/', questionsController.fetchAllQuestions)//fetch all questions
-// router.get('/:id', questionsController.fetchQuestionById)//fetch a specific question
-// router.delete('/:id', checkAuthMiddleware.checkAuth, questionsController.deleteQuestion)//delete a question
 
 module.exports = router;
